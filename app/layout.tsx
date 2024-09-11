daa24f45fd66a234216d954691dcca08d5829e8a
Marcos Ramos <marcosperes.contato@gmail.com>
2024-09-11 10:31:45 -0300

    Export of layout page metadata has been refactored

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
-
+import type { Viewport } from 'next'
+ 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 export const metadata = {
   title: 'Meu Portfólio | Marcos',
   description: 'Aqui eu te conto sobre meu progresso nos estudos em programação',
