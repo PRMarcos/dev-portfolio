1735bbb1c73b9de1930c53d5f913c1daf21cbcce
Marcos Ramos <marcosperes.contato@gmail.com>
2024-09-19 17:09:44 -0300

    Page to show post list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,19 @@
+import { PostPreviewLinkList } from "@/components/PostPreviewLinkList";
+import {data } from "@/site-data/posts/fakepostdata";
+
 export default function Blog() {
 
-    return (
-        <main
-          className={`w-full`}
-        >
-  
-        </main>
-    );
-  }
+  return (
+    <div
+      className={`w-full mb-9`}
+    >
+   
+        <h2 className="font-bold text-xl mb-5">{data.length==0? "Nada para mostrar ainda...": "Posts"}</h2>
+
+       <PostPreviewLinkList postList={data} />
+
+   
+
+    </div>
+  );
+}
