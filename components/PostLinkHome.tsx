aea2afe70f346bc3f2880977f86685eae6b1d018
Marcos Ramos <marcosperes.contato@gmail.com>
2024-09-19 17:05:46 -0300

    fixed href to point to the correct path

--- components/PostLinkHome.tsx
+++ components/PostLinkHome.tsx
@@ -1,9 +1,8 @@
 export function PostLinkHome({date,slug,title}:{date:string,slug:string,title:string}){
-// tratar data para formato diferente
     return(
         <div className=" my-2 flex gap-2 ">
         <span className="flex-none">{date}</span>
-        <a href={`/${slug}`} className=" hover:text-[#0582b6]">
+        <a href={`/blog/${slug}`} className=" hover:text-[#0582b6]">
           <h3 className="font-bold">{title}</h3>
         </a>
     </div>
