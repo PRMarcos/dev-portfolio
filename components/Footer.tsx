561640bd060f8e58596ee75218bc9e36d6d0d9b4
Marcos Ramos <marcosperes.contato@gmail.com>
2024-09-19 18:12:28 -0300

    footer message new comes from site data file

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+import { layout_footerMessage } from "@/site-data";
+
 export default function Footer({ className }: { className?: string }) {
   return (
     <footer
       className={`h-9 w-full flex justify-center items-center text-center text-[11px] text-gray-400 ${className}`}
     >
-      <span> 2023 © Marcos Peres Ramos. Todos os direitos reservados.</span>
+      <span>{layout_footerMessage}</span>
      
     </footer>
   );
